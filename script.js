07e934e8e73e053ae94cfcc2c9813a9420ae9c9f
MehtabSJ003 <mehtabsj003@gmail.com>
2023-12-24 22:56:33 -0500

    added js

---
+++ script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+//create an array of all the months of year
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July ",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+// Selecting elements
+const form = document.querySelector(".form");
+const containerWorkouts = document.querySelector(".workouts");
+const inputType = document.querySelector(".form--input-type");
+const inputDistance = document.querySelector(".form--input-distance");
+const inputDuration = document.querySelector(".form--input-duration");
+const inputCadence = document.querySelector(".form--input-cadence");
+const inputElevation = document.querySelector(".form--input-elevation");
