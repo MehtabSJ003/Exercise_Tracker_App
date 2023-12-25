5c1d51ccf607d922eb0f3fc34d2311f0c7e26e27
MehtabSJ003 <mehtabsj003@gmail.com>
2023-12-24 23:39:08 -0500

    loaded map

--- script.js
+++ script.js
@@ -24,3 +24,30 @@ const inputDistance = document.querySelector(".form--input-distance");
 const inputDuration = document.querySelector(".form--input-duration");
 const inputCadence = document.querySelector(".form--input-cadence");
 const inputElevation = document.querySelector(".form--input-elevation");
+
+// Make page get location
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(
+        `https://www.google.com/maps/@${latitude},${longitude},15z?entry=ttu`
+      );
+
+      const map = L.map("map").setView([51.505, -0.09], 13);
+
+      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker([51.5, -0.09])
+        .addTo(map)
+        .bindPopup("A pretty CSS popup.<br> Easily customizable.")
+        .openPopup();
+    },
+    function () {
+      alert("Could not get your position");
+    }
+  );
